fde22d6b47c0a6edacdcc510b20aa8b6fa51a683
cmw9957 <cmw9957@gmail.com>
2025-02-28 01:40:36 +0900

    fix : initialize logic error

--- src/SwapProxy.sol
+++ src/SwapProxy.sol
@@ -7,10 +7,26 @@ import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.so
 contract SwapProxy {
     address public admin;
     address public proxy;
-    constructor(address _implementation) {
+
+    constructor(address _implementation, address _token0, address _token1, address _token2) {
+        // ProxyAdmin 인스턴스 생성
         ProxyAdmin adminInstance = new ProxyAdmin(msg.sender);
         admin = address(adminInstance);
-        TransparentUpgradeableProxy proxyInstance = new TransparentUpgradeableProxy(_implementation, admin, "");
-        proxy = address(proxyInstance);
+
+        // TransparentUpgradeableProxy 생성, data 인자로 initialize 함수 호출
+        bytes memory initializeData = abi.encodeWithSignature(
+            "initialize(address,address,address)", 
+            _token0, 
+            _token1, 
+            _token2
+        );
+
+        TransparentUpgradeableProxy proxyInstance = new TransparentUpgradeableProxy(
+            _implementation,  // _implementation 주소
+            admin,            // 관리자로 ProxyAdmin 설정
+            initializeData    // 초기화 함수 호출을 위한 data
+        );
+        
+        proxy = address(proxyInstance);  // 프록시 주소 저장
     }
 }
