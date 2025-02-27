ee9039c8a5ec70ced72a0eda732455dd7e14f54f
cmw9957 <cmw9957@gmail.com>
2025-02-27 17:25:25 +0900

    feat : proxy contract

--- src/SwapProxy.sol
+++ src/SwapProxy.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10;
 
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract SwapProxy is TransparentUpgradeableProxy {
-    constructor(
-        address _logic,
-        address _admin,
-        bytes memory _data
-    ) TransparentUpgradeableProxy(_logic, _admin, _data) {}
+contract SwapProxy {
+    address public admin;
+    address public proxy;
+    constructor(address _implementation) {
+        ProxyAdmin adminInstance = new ProxyAdmin(msg.sender);
+        admin = address(adminInstance);
+        TransparentUpgradeableProxy proxyInstance = new TransparentUpgradeableProxy(_implementation, admin, "");
+        proxy = address(proxyInstance);
+    }
 }
