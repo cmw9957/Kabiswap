e99cc88f40f69678360bfcf39370b02d78eb4841
cmw9957 <cmw9957@gmail.com>
2025-02-27 17:25:47 +0900

    add : import

--- src/KabiLPtoken.sol
+++ src/KabiLPtoken.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KabiLPtoken is ERC20 {
-    constructor() ERC20("Kabi Liquidity Pool Token", "KLT") {}
+contract KabiLPtoken is ERC20, ERC20Permit, Ownable {
+    constructor() ERC20("Kabi Liquidity Pool Token", "KLT") Ownable(msg.sender) ERC20Permit("Kabi Liquidity Pool Token"){}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
--- src/UpsideERC20.sol
+++ src/UpsideERC20.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract UpsideERC20 is ERC20 {
-    constructor() ERC20("Upside Pegging Token", "UPT") {
+contract UpsideERC20 is ERC20, ERC20Permit, Ownable {
+    constructor() ERC20("Upside Pegging Token", "UPT") Ownable(msg.sender) ERC20Permit("Kabi Liquidity Pool Token") {
         _mint(msg.sender, 100 ether);
     }
 
