a0e83990450867453231b091d6de76cbe55a1b68
cmw9957 <cmw9957@gmail.com>
2025-02-27 16:24:42 +0900

    add : ERC20PErmit

--- src/KabiswapERC20.sol
+++ src/KabiswapERC20.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KabiswapERC20 is ERC20, Ownable {
+contract KabiswapERC20 is ERC20, ERC20Permit, Ownable {
     uint256 public immutable CAP;
     address private beanDao;
 
-    constructor() ERC20("Kabi Swap Token", "KST") Ownable(msg.sender) {
+    constructor() ERC20("Kabi Swap Token", "KST") Ownable(msg.sender) ERC20Permit("Kabi Swap Token") {
         CAP = 1_000_000_000 ether;
         _mint(msg.sender, 100_000 ether);
     }
