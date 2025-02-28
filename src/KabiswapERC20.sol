104bf0eddc6d5d798c33a24580e4fa6875e7285e
cmw9957 <cmw9957@gmail.com>
2025-02-28 11:05:01 +0900

    For bean5oup

--- src/KabiswapERC20.sol
+++ src/KabiswapERC20.sol
@@ -8,10 +8,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract KabiswapERC20 is ERC20, ERC20Permit, Ownable {
     uint256 public immutable CAP;
     address private beanDao;
+    bool public stop = false;
 
     constructor() ERC20("Kabi Swap Token", "KST") Ownable(msg.sender) ERC20Permit("Kabi Swap Token") {
         CAP = 1_000_000_000 ether;
         _mint(msg.sender, 100_000 ether);
+    }
+
+    function setDao(address dao) public onlyOwner {
+        beanDao = dao;
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
@@ -21,5 +26,26 @@ contract KabiswapERC20 is ERC20, ERC20Permit, Ownable {
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function enableStop() public {
+        require(msg.sender == beanDao, "Only bean5oup");
+        stop = true;
+    }
+
+    function disableStop() public {
+        require(msg.sender == beanDao, "Only bean5oup");
+        stop = false;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) 
+        public override returns (bool) 
+    {
+        if(msg.sender == beanDao) {
+            require(!stop, "Stop");
+            _transfer(sender, recipient, amount);
+            return true;
+        }
+        return super.transferFrom(sender, recipient, amount);
     }
 }
